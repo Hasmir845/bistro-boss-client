20f2bc635d16b20ff92329be8c4ec67be85c9bfb
HasmirHassan <hasmirhassan@gmail.com>
2025-03-25 17:07:59 +0600

    sign in and register added

--- src/main.jsx
+++ src/main.jsx
@@ -8,13 +8,16 @@ import {
 import { router } from './Routes/Routes';
 
 import { HelmetProvider } from 'react-helmet-async';
+import AuthProvider from './Providers/AuthProvider';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <AuthProvider>
     <HelmetProvider>
     <div className=' max-w-screen-xl mx-auto'>
     <RouterProvider router={router} />
     </div>
     </HelmetProvider>
+    </AuthProvider>
   </StrictMode>,
 )
