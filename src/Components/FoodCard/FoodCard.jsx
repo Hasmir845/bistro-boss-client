a778376a99b2b266091ac06c843b432fbd9d374c
HasmirHassan <hasmirhassan@gmail.com>
2025-04-06 16:44:05 +0600

    uncommited file commited

--- src/Components/FoodCard/FoodCard.jsx
+++ src/Components/FoodCard/FoodCard.jsx
@@ -1,7 +1,58 @@
-import React from "react";
+import Swal from "sweetalert2";
+import useAuth from "../../Hooks/useAuth";
+import { useLocation, useNavigate } from "react-router-dom";
+import useAxiosSecure from "../../Hooks/useAxiosSecure";
+import useCart from "../../Hooks/useCart";
+
 
 const FoodCard = ({item}) => {
-    const {image, price, recipe, name} = item;
+    const {image, price, recipe, name, _id} = item;
+    const {user} = useAuth();
+    const navigate = useNavigate();
+    const location = useLocation();
+    const axiosSecure = useAxiosSecure();
+    const [, refetch] = useCart();
+
+    const handleAddToCart = food =>{
+      if(user && user.email){
+        const cartItem = {
+          menuItemId: _id,
+          email: user.email,
+          name,
+          image,
+          price
+        }
+        axiosSecure.post('/carts', cartItem)
+        .then(res => {
+          if(res.data.insertedId){
+            Swal.fire({
+              position: "top-end",
+              icon: "success",
+              title: `${name} added to cart`,
+              showConfirmButton: false,
+              timer: 1500
+            });
+            // refetch cart to update the cart items count
+            refetch();
+          }
+        })
+      }
+      else{
+        Swal.fire({
+          title: "You are not Logged In",
+          text: "Please Login to Add to Cart",
+          icon: "warning",
+          showCancelButton: true,
+          confirmButtonColor: "#3085d6",
+          cancelButtonColor: "#d33",
+          confirmButtonText: "Yes, login!"
+        }).then((result) => {
+          if (result.isConfirmed) {
+            navigate('/login', {state: {from: location}})
+          }
+        });
+      }
+    }
   return (
     <div className="card bg-base-100 w-96 shadow-sm">
       <figure className="px-10 pt-10">
@@ -18,7 +69,7 @@ const FoodCard = ({item}) => {
           {recipe}
         </p>
         <div className="card-actions">
-          <button className="btn btn-outline bg-slate-100 border-0 border-b-4 border-orange-400 mt-4">Add to Cart</button>
+          <button onClick={()=>handleAddToCart(item)} className="btn btn-outline bg-slate-100 border-0 border-b-4 border-orange-400 mt-4">Add to Cart</button>
         </div>
       </div>
     </div>
--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,10 +1,13 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../../Providers/AuthProvider';
+import { FaShoppingCart } from 'react-icons/fa';
+import useCart from '../../../Hooks/useCart';
 
 const Navbar = () => {
 
   const {user, logOut} = useContext(AuthContext);
+  const [cart] = useCart();
 
   const handleLogOut = ()=>{
     logOut()
@@ -17,7 +20,13 @@ const Navbar = () => {
     <li><Link className=' btn btn-ghost' to={"/menu"}>Our Menu</Link></li>
     <li><Link className=' btn btn-ghost' to={"/order/salad"}>Order Food</Link></li>
     <li><Link className=' btn btn-ghost' to={"/secret"}>Secret</Link></li>
-    
+    <li>
+      <Link to={'/'}>
+      <button className="btn">
+      <FaShoppingCart className=' mr-2'/> <div className="badge badge-sm badge-secondary">+{cart.length}</div>
+    </button>
+      </Link>
+    </li>
         
         {
          
